89e65cd914905886e7a265929204faccbf6fcca8
Coffee <forstbloom56@gmail.com>
2026-07-23 17:28:09 -0400

    Update Tab.lua

--- Layout/Tab.lua
+++ Layout/Tab.lua
@@ -19,21 +19,42 @@ function Tab.new(window, name)
 
 	self.Button.Parent = window.Main
 
-	self.Container = Utility:Create("Frame", {
+	self.Container = Utility:Create("ScrollingFrame", {
 		Size = UDim2.new(1, -20, 1, -70),
 		Position = UDim2.fromOffset(10, 55),
-		BackgroundTransparency = 1
+		BackgroundTransparency = 1,
+		BorderSizePixel = 0,
+		ScrollBarThickness = 2
 	})
 
 	self.Container.Parent = window.Main
 
+	self.Layout = Instance.new("UIListLayout")
+	self.Layout.Padding = UDim.new(0, 8)
+	self.Layout.Parent = self.Container
+
+	self.Button.MouseButton1Click:Connect(function()
+		self:Show()
+	end)
+
 	return self
 end
+
+
+function Tab:Show()
+	for _, tab in pairs(self.Window.Tabs or {}) do
+		tab.Container.Visible = false
+	end
+
+	self.Container.Visible = true
+end
+
 
 function Tab:CreateSection(name)
 	local Section = require(script.Parent.Section)
 
 	return Section.new(self, name)
 end
+
 
 return Tab
