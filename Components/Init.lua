4db3ce2efc539f7e48bdc1308847d7db9e3a65ec
Coffee <forstbloom56@gmail.com>
2026-07-23 17:33:08 -0400

    Create Init.lua

---
+++ Components/Init.lua
@@ -0,0 +1,9 @@
+local Components = {}
+
+Components.Button = require(script.Button)
+Components.Toggle = require(script.Toggle)
+Components.Slider = require(script.Slider)
+Components.Dropdown = require(script.Dropdown)
+Components.Notification = require(script.Notification)
+
+return Components
