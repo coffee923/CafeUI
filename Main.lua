fe59e0a1aead72bb082726eacb3f424c5c1b31b1
Coffee <forstbloom56@gmail.com>
2026-07-23 17:34:31 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,20 +2,33 @@ local CafeUI = {}
 
 CafeUI.Version = "1.0"
 
+CafeUI.Theme = require(script.Core.Theme)
+CafeUI.Animation = require(script.Core.Animation)
+CafeUI.Utility = require(script.Core.Utility)
+CafeUI.Device = require(script.Core.Device)
+
+CafeUI.Components = require(script.Components.Init)
+CafeUI.Layout = require(script.Layout.Init)
+
 function CafeUI:CreateWindow(config)
 	local Window = require(script.Core.Window)
 
 	return Window.new(config or {})
 end
 
-function CafeUI:GetTheme()
-	return require(script.Core.Theme)
+function CafeUI:SetTheme(theme)
+	self.Theme:Set(theme)
+end
+
+function CafeUI:GetTheme(theme)
+	return self.Theme:Get(theme)
 end
 
 function CafeUI:Notify(window, config)
-	local Notification = require(script.Components.Notification)
-
-	Notification:Create(window, config)
+	if window then
+		local Notification = self.Components.Notification
+		Notification:Create(window, config)
+	end
 end
 
 return CafeUI
